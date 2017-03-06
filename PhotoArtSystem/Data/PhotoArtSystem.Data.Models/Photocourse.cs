bd09509915b15f33ee9c03c20444e8c3be5e1db5
Borayvor <Boray@abv.bg>
2017-03-06 22:08:03 +0200

    start db design

---
+++ PhotoArtSystem/Data/PhotoArtSystem.Data.Models/Photocourse.cs
@@ -0,0 +1,14 @@
+﻿namespace PhotoArtSystem.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using Common.Models;
+    using PhotoArtSystem.Common.Constants;
+
+    public class Photocourse : BaseModelGuid, IAuditInfo, IDeletableEntity
+    {
+        [Required]
+        [MaxLength(ModelConstants.PhotocourseNameMaxLength)]
+        [MinLength(ModelConstants.PhotocourseNameMinLength)]
+        public string Name { get; set; }
+    }
+}
