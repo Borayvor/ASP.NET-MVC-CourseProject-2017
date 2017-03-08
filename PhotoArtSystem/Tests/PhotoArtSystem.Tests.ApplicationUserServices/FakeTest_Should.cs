5b1210d42910883b88744bc74a8ec243c82c5cd1
Borayvor <Boray@abv.bg>
2017-03-08 23:21:29 +0200

    add fake test

---
+++ PhotoArtSystem/Tests/PhotoArtSystem.Tests.ApplicationUserServices/FakeTest_Should.cs
@@ -0,0 +1,19 @@
+﻿namespace PhotoArtSystem.Tests.ApplicationUserServices
+{
+    using NUnit.Framework;
+    using Telerik.JustMock;
+
+    [TestFixture]
+    public class FakeTest_Should
+    {
+        [Test]
+        public void Work()
+        {
+            // Arrange
+            var number = 1;
+
+            // Act // Assert
+            Mock.Assert(number == 1);
+        }
+    }
+}
