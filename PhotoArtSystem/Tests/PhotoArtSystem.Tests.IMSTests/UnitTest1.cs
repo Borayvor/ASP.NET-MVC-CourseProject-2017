2bc2d891974ae45863ee48111cae211c3e4e9e95
Borayvor <Boray@abv.bg>
2017-03-11 16:23:50 +0200

    add IMSTest to test jenkins

---
+++ PhotoArtSystem/Tests/PhotoArtSystem.Tests.IMSTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿namespace PhotoArtSystem.Tests.IMSTests
+{
+    using System.Linq;
+    using Data;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            // Act
+            int usersCount = context.Users.Count();
+
+            // Assert
+            Assert.AreEqual(1, usersCount);
+        }
+    }
+}
