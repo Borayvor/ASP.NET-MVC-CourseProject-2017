95fd5b4bf97a3551a78a26a95b09b6c9c2b79b97
Borayvor <Boray@abv.bg>
2017-03-12 00:46:13 +0200

    remove modernizr and add Submit HtmlHelper

---
+++ PhotoArtSystem/Web/PhotoArtSystem.Web.Infrastructure/HtmlHelpers/Common.cs
@@ -0,0 +1,17 @@
+﻿namespace PhotoArtSystem.Web.Infrastructure.HtmlHelpers
+{
+    using System.Web.Mvc;
+
+    public static class Common
+    {
+        public static MvcHtmlString Submit(this HtmlHelper helper, string value, object htmlAttributes = null)
+        {
+            var submitButton = new TagBuilder("input");
+            submitButton.Attributes.Add("type", "submit");
+            submitButton.Attributes.Add("value", value);
+            submitButton.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+
+            return new MvcHtmlString(submitButton.ToString());
+        }
+    }
+}
