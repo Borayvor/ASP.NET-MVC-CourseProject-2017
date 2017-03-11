33ee1c2e75c1afe77c2f4bcdbbd14d7f52401b47
Borayvor <Boray@abv.bg>
2017-03-12 00:50:48 +0200

    add default value

--- PhotoArtSystem/Web/PhotoArtSystem.Web.Infrastructure/HtmlHelpers/Common.cs
+++ PhotoArtSystem/Web/PhotoArtSystem.Web.Infrastructure/HtmlHelpers/Common.cs
@@ -4,7 +4,7 @@
 
     public static class Common
     {
-        public static MvcHtmlString Submit(this HtmlHelper helper, string value, object htmlAttributes = null)
+        public static MvcHtmlString Submit(this HtmlHelper helper, string value = "Submit", object htmlAttributes = null)
         {
             var submitButton = new TagBuilder("input");
             submitButton.Attributes.Add("type", "submit");
