f346526be026c99ccd2d866a0ebcd692a5db666f
Borayvor <Boray@abv.bg>
2017-03-21 23:27:59 +0200

    change test

--- PhotoArtSystem/Tests/PhotoArtSystem.Tests.IntegrationTests/DummyContextTest.cs
+++ PhotoArtSystem/Tests/PhotoArtSystem.Tests.IntegrationTests/DummyContextTest.cs
@@ -8,13 +8,14 @@
     public class DummyContextTest
     {
         [Test]
-        public void TestMethod()
+        [TestCase("admin@admin.com")]
+        public void Db_ShouldHaveOnlyOneUserWithSuchEmail(string email)
         {
             // Arrange
             ApplicationDbContext context = new ApplicationDbContext();
 
             // Act
-            int usersCount = context.Users.Count();
+            int usersCount = context.Users.Where(x => x.Email == email).Count();
 
             // Assert
             Assert.AreEqual(1, usersCount);
