d46a73e676c968578886816b3a5e47fb87fe68cc
Borayvor <Boray@abv.bg>
2017-03-27 01:35:27 +0300

    start adding Administration

---
+++ PhotoArtSystem/Web/PhotoArtSystem.Web/Areas/Administration/Controllers/BaseAdminController.cs
@@ -0,0 +1,16 @@
+﻿namespace PhotoArtSystem.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+    using Common.Constants;
+    using Services.Web.Contracts;
+    using Web.Controllers;
+
+    [Authorize(Roles = AuthConstants.AdministratorRoleName)]
+    public abstract class BaseAdminController : BaseController
+    {
+        public BaseAdminController(ICacheService cache)
+            : base(cache)
+        {
+        }
+    }
+}
