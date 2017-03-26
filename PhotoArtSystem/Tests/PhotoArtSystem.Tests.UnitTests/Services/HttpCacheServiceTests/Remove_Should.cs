33c8b8c67405108515a9f277a70271ca884f1300
Borayvor <Boray@abv.bg>
2017-03-26 21:39:06 +0300

    add one test

---
+++ PhotoArtSystem/Tests/PhotoArtSystem.Tests.UnitTests/Services/HttpCacheServiceTests/Remove_Should.cs
@@ -0,0 +1,23 @@
+﻿namespace PhotoArtSystem.Tests.UnitTests.Services.HttpCacheServiceTests
+{
+    using Common.Constants;
+    using NUnit.Framework;
+    using PhotoArtSystem.Services.Web;
+
+    [TestFixture]
+    public class Remove_Should
+    {
+        [Test]
+        public void Throw_ArgumentNullException_WithProperMessage_When_ItemName_IsNull()
+        {
+            // Arange
+            var service = new HttpCacheService();
+
+            // Act & Assert
+            Assert.That(
+                () => service.Remove(null),
+                            Throws.ArgumentNullException.With.Message.Contains(
+                                GlobalConstants.ItemNameRequiredExceptionMessage));
+        }
+    }
+}
